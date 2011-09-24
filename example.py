37c95ffd0ac2bd7bb45dd96e8ce22e4701612138
Jan David Mol <mol@astron.nl>
2011-09-24 11:42:02 +0000

    simplified example slightly

--- example.py
+++ example.py
@@ -6,26 +6,27 @@ f = BeamFormedFile.BeamFormedFile("test.h5")
 print "Observation %s ran from %s UTC to %s UTC" % (f.observationID().get(), f.observationStartUTC().get(), f.observationEndUTC().get())
 
 for sapnr in count():
-  if not f.sap(sapnr).exists():
+  sap = f.sap(sapnr)
+
+  if not sap.exists():
     break
 
-  sap = f.sap(sapnr)
 
   print "SAP %u points to (%.2f, %.2f)" % (sapnr, sap.pointRA().get(), sap.pointDEC().get())
 
   for beamnr in count():
-    if not sap.beam(beamnr).exists():
-      break
-
     beam = sap.beam(beamnr)
+
+    if not beam.exists():
+      break
 
     print "    Beam %u points to (%.2f, %.2f)" % (beamnr, beam.pointRA().get(), beam.pointDEC().get())
 
     for stokesnr in count():
-      if not beam.stokes(stokesnr).exists():
-        break
-
       stokes = beam.stokes(stokesnr)
+
+      if not stokes.exists():
+        break
 
       print "        Stokes %s has %u subbands and %u samples" % (stokes.stokesComponent().get(), stokes.nofSubbands().get(), stokes.nofSamples().get())
 
