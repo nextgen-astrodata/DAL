e73a9313fa93099c237be59b95fa711c02d3a72e
Jan David Mol <mol@astron.nl>
2011-11-07 15:26:04 +0000

    added library version check

---
+++ src/lda_version.cc
@@ -0,0 +1,10 @@
+#include "lda_config.h"
+#include "lda_version.h"
+
+namespace LDA {
+
+std::string get_lib_version() {
+  return LDA_VERSION;
+}
+
+}
---
+++ src/lda_version.h
@@ -0,0 +1,13 @@
+#ifndef __LDAVERSION__
+#define __LDAVERSION__
+
+#include <string>
+
+namespace LDA {
+
+std::string get_lib_version();
+
+}
+
+#endif
+
