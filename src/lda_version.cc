462702842f50f4cddfcedd8a9e3f0cfaf7372962
Jan David Mol <mol@astron.nl>
2011-11-11 09:47:54 +0000

    Provide functions for HDF5 version checks

--- src/lda_version.cc
+++ src/lda_version.cc
@@ -1,10 +1,29 @@
 #include "lda_config.h"
 #include "lda_version.h"
+#include <cstdio>
+#include <hdf5.h>
 
 namespace LDA {
 
-std::string get_lib_version() {
+using namespace std;
+
+string get_lib_version() {
   return LDA_VERSION;
+}
+
+string get_lda_hdf5_version() {
+  return H5_PACKAGE_VERSION;
+}
+
+string get_current_hdf5_lib_version() {
+  char buf[128];
+  unsigned maj, min, rel;
+
+  H5get_libversion(&maj, &min, &rel);
+
+  snprintf(buf, sizeof buf, "%u.%u.%u", maj, min, rel);
+
+  return string(buf);
 }
 
 }
--- src/lda_version.h
+++ src/lda_version.h
@@ -2,10 +2,55 @@
 #define __LDAVERSION__
 
 #include <string>
+#include <cstdio>
+#include <hdf5.h>
 
 namespace LDA {
 
+/*
+ * Return the version of this LDA library.
+ */
 std::string get_lib_version();
+
+/*
+ * Return the version of HDF5 used to compile the LDA.
+ */
+std::string get_lda_hdf5_version();
+
+/*
+ * Return the version of HDF5 headers currently in use.
+ */
+std::string get_current_hdf5_header_version() {
+  // this code needs to be in a header file, to let
+  // the client compiler fill in H5_PACKAGE_VERSION below.
+
+  return std::string(H5_PACKAGE_VERSION);
+}
+
+/*
+ * Return the version of HDF5 library currently in use.
+ */
+std::string get_current_hdf5_lib_version();
+
+/*
+ * Returns true if the hdf5 versions used to compile
+ * the LDA and to compile the client code are the same.
+ */
+bool check_hdf5_versions() {
+  // this code needs to be in a header file, to let
+  // the client compiler fill in H5_PACKAGE_VERSION.
+
+  // client HDF5 header should be the same version as used by LDA
+  if (get_current_hdf5_header_version() != get_lda_hdf5_version())
+    return false;
+
+  // client HDF5 library should be the same version as used by LDA
+  if (get_current_hdf5_lib_version() != get_lda_hdf5_version())
+    return false;
+
+  // all hdf5 versions used are the same
+  return true;  
+}
 
 }
 
