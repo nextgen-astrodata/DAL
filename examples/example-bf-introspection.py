d524c9171ccd7d0f027ea74caf3a6113ed667180
Jan David Mol <mol@astron.nl>
2012-05-10 14:39:09 +0000

    Added introspection example

---
+++ examples/example-bf-introspection.py
@@ -0,0 +1,22 @@
+import DAL
+
+f = DAL.BF_File("test.h5")
+
+print "file %s reports to be version %s" % (f.filename, f.fileVersion())
+print
+
+print "supp   = whether this attribute is supported by the version as reported by the file"
+print "exists = whether this attribute exists in the file"
+print "valid  = whether this attribute can be read (exists and is of the correct type)"
+print
+
+print "%40s %6s %6s %6s %s" % ("HDF5 Node name", "supp", "exists", "valid", "value")
+
+for n in f.nodeNames():
+  node = f.getNode(n)
+
+  if node.exists():
+    print "%40s %6s %6s %6s %s" % (n, node.supported(), node.exists(), node.valid(), node.value)
+  else:  
+    print "%40s %6s %6s %6s %s" % (n, node.supported(), node.exists(), "", "")
+
