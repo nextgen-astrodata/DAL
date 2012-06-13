fa11e561d845f67fa3dd2ff2f9fefeb804a19273
Alexander S. van Amesfoort <amesfoort@astron.nl>
2012-06-13 20:31:38 +0200

    Complete C++ TBB read/print example program.

--- examples/example-tbb-read.cc
+++ examples/example-tbb-read.cc
@@ -9,16 +9,44 @@
 
 #include "dal/lofar/TBB_File.h"
 
+#define MAX_PRINTED 64
+
 using namespace std;
 
 static void readTBBFile(const string& filename) {
 	DAL::TBB_File tf(filename);
 
-	vector<DAL::TBB_Station> stations = tf.stations();
+	const vector<size_t> pos(1, 0); // = {0}: always read starting at idx 0
+	int16_t* data = NULL;
+	size_t dataLen = 0;
 
+	vector<DAL::TBB_Station> stations = tf.stations();
 	for (size_t i = 0; i < stations.size(); i++) {
-		cout << "Station: " << stations[i].stationName().get() << endl;
+		cout << "Station " << stations[i].stationName().get() << ":" << endl;
+
+		vector<DAL::TBB_DipoleDataset> dipoles = stations[i].dipoles();
+		for (size_t j = 0; j < dipoles.size(); j++) {
+			cout << "\tDipole " << dipoles[j].rspID().get() << " " << dipoles[j].rcuID().get() << ":" << endl;
+
+			size_t dpDataLen = dipoles[j].dims()[0];
+			if (dataLen < dpDataLen) {
+				delete[] data;
+				dataLen = dpDataLen;
+				data = new int16_t[dataLen];
+			}
+
+			dipoles[j].get1D(pos, dataLen, data);
+
+			size_t printedLen = dataLen < MAX_PRINTED ? dataLen : MAX_PRINTED;
+			for (size_t k = 0; k < printedLen; k++) {
+				cout << data[k] << " ";
+			}
+			cout << endl;
+		}
+
 	}
+
+	delete[] data; // not reached if exc thrown
 }
 
 static void printUsage(const char* progname) {
