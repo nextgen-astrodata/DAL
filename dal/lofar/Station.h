c8b9017e64843acb2a14d6269072a64f5b181621
alxames <amesfoort@astron.nl>
2012-07-31 00:28:36 +0200

    Adapt Station.h comment for doxygen.

--- dal/lofar/Station.h
+++ dal/lofar/Station.h
@@ -6,13 +6,13 @@
 
 namespace DAL {
 
-/*
+/*!
  * Returns the LOFAR station name (e.g. "CS011") corresponding to stationID.
  * Returns "ST<stationID>" (always 5 characters) if stationID is unknown.
  */
 std::string stationIDToName(unsigned stationID);
 
-/*
+/*!
  * Returns LOFAR station ID corresponding to stationName (e.g. "CS011" or "CS011HBA0", same result).
  * Throws DAL::DALValueError if stationName contains an invalid number.
  */
