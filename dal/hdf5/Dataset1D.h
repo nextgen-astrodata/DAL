010d8b742d05342796e319acc3257f698c286ad6
alxames <amesfoort@astron.nl>
2012-08-09 01:13:02 +0200

    Add lgpl3 headers to *.h *.c *.tcc

--- dal/hdf5/Dataset1D.h
+++ dal/hdf5/Dataset1D.h
@@ -1,3 +1,19 @@
+/* Copyright 2011-2012  ASTRON, Netherlands Institute for Radio Astronomy
+ * This file is part of the Data Access Library (DAL).
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either 
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef DAL_DATASET1D_H
 #define DAL_DATASET1D_H
 
