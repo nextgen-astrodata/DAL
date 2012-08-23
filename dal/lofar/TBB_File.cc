9c474705fcc60ffccb082df57b9ce7821f92caec
alxames <amesfoort@astron.nl>
2012-08-23 11:35:37 +0200

    Revert "Plug obscure H5F leak in File sub-class constr."
    This reverts commit c949a3e61910cbf6d967fe6022588c68f186f31c.
    
    This is not needed. C++ calls the destructor of a fully constructed
    sub-object, member, or super-class before entering the exc handler.

--- dal/lofar/TBB_File.cc
+++ dal/lofar/TBB_File.cc
@@ -23,7 +23,7 @@ namespace DAL {
 TBB_File::TBB_File() {}
 
 TBB_File::TBB_File( const std::string &filename, enum fileMode mode )
-try :
+:
   CLA_File(filename, mode)
 {
   if (mode == CREATE) {
@@ -38,10 +38,6 @@ try :
       throw DALException("Failed to open TBB file: A TBB file must have /FILETYPE=\"tbb\".");
     }
   }
-} catch (...)
-{
-  close();
-  // will rethrow
 }
 
 TBB_File::~TBB_File() {}
