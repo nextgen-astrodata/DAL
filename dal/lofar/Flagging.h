c3fdc5fae8de8901f85a69ccc47e72966e22c05e
alxames <amesfoort@astron.nl>
2012-08-24 15:50:11 +0200

    Add Range struct.

---
+++ dal/lofar/Flagging.h
@@ -0,0 +1,36 @@
+/* Copyright 2011-2012  ASTRON, Netherlands Institute for Radio Astronomy
+ * This file is part of the Data Access Library (DAL).
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either 
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cstddef>
+#include "dal/hdf5/types/h5tuple.h"
+
+namespace DAL {
+
+struct Range : public TupleBase<unsigned long long, 2> {
+  unsigned long long begin; // inclusive
+  unsigned long long end;   // exclusive
+
+
+  Range() : begin(0), end(0) {}
+
+  Range(unsigned long long begin, unsigned long long end) : begin(begin), end(end) {}
+
+  // Note: size() always returns 2 (number of tuple elements)
+};
+
+}
+
