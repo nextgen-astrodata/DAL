9ec6b5b3c9ad3ff11eab700f94f446d4cb9173ea
alxames <amesfoort@astron.nl>
2012-08-31 22:42:34 +0200

    cmake: all tests under BUILD_TESTING, simple test cases first, add config prints

--- cmake/TestMacros.cmake
+++ cmake/TestMacros.cmake
@@ -1,17 +1,23 @@
 macro(add_c_test _name)
-  add_executable(${_name} ${_name}.cc ${ARGN})
-  target_link_libraries(${_name} lofardal)
-  add_test(${_name} ${CMAKE_CURRENT_BINARY_DIR}/${_name})
+  if(BUILD_TESTING)
 
-  if(VALGRIND_FOUND)
-    add_test("vg-${_name}" ${VALGRIND_PROGRAM} ${CMAKE_CURRENT_BINARY_DIR}/${_name})
-  endif(VALGRIND_FOUND)
+    add_executable(${_name} ${_name}.cc ${ARGN})
+    target_link_libraries(${_name} lofardal)
+    add_test(${_name} ${CMAKE_CURRENT_BINARY_DIR}/${_name})
+
+    if(VALGRIND_FOUND)
+      add_test("vg-${_name}" ${VALGRIND_PROGRAM} ${CMAKE_CURRENT_BINARY_DIR}/${_name})
+    endif(VALGRIND_FOUND)
+
+  endif(BUILD_TESTING)
 endmacro(add_c_test)
 
-if(PYTHON_BINDINGS AND PYTHON_EXECUTABLE)
-  macro(add_py_test _name _pyfile)
+macro(add_py_test _name _pyfile)
+  if(BUILD_TESTING AND PYTHON_BINDINGS AND PYTHON_EXECUTABLE)
+
     add_test(${_name} ${PYTHON_EXECUTABLE} ${_pyfile} ${ARGN})
     set_tests_properties(${_name} PROPERTIES ENVIRONMENT "PYTHONPATH=${CMAKE_BINARY_DIR}/dal")
-  endmacro(add_py_test)
-endif(PYTHON_BINDINGS AND PYTHON_EXECUTABLE)
+
+  endif(BUILD_TESTING AND PYTHON_BINDINGS AND PYTHON_EXECUTABLE)
+endmacro(add_py_test)
 
