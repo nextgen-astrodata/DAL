633daf7a5cca71e620e02c3593457ffec2107b5b
Jan David Mol <mol@astron.nl>
2012-08-31 13:11:19 +0200

    Allow parameters to be passed to python tests

--- cmake/TestMacros.cmake
+++ cmake/TestMacros.cmake
@@ -12,7 +12,7 @@ endmacro(add_c_test)
 
 if(PYTHON_BINDINGS AND PYTHON_EXECUTABLE)
   macro(add_py_test _name _pyfile)
-    add_test(${_name} ${PYTHON_EXECUTABLE} ${_pyfile})
+    add_test(${_name} ${PYTHON_EXECUTABLE} ${_pyfile} ${ARGN})
     set_tests_properties(${_name} PROPERTIES ENVIRONMENT "PYTHONPATH=${CMAKE_BINARY_DIR}/dal")
   endmacro(add_py_test)
 endif(PYTHON_BINDINGS AND PYTHON_EXECUTABLE)
